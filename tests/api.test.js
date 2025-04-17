5b56c961a610de0e9554c2da7d65858ef50f3ad0
Kirsty <kirsty.butterworth@northcoders.com>
2025-04-17 14:23:26 +0100

    updated test

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,17 +1,16 @@
 const request = require("supertest");
 
-const BASE_URL = "https://web-cdn.api.bbci.co.uk";
+const BASE_URL =
+  "https://web-cdn.api.bbci.co.uk/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:";
 
 describe("Scenario 1: Status code and response time", () => {
   test("should return a status 200 and respond in under 1000ms", () => {
     const startTime = Date.now();
-    const responseTime = Date.now() - startTime;
     return request(BASE_URL)
-      .get(
-        "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations"
-      )
+      .get("six-nations")
       .expect(200)
       .then(() => {
+        const responseTime = Date.now() - startTime;
         expect(responseTime).toBeLessThan(1000);
       });
   });
@@ -19,9 +18,7 @@ describe("Scenario 1: Status code and response time", () => {
 describe("Scenario 2: Validate the ID and participant count", () => {
   test('should return only valid ID"s, never null or an empty string and a participants array length of 6', () => {
     return request(BASE_URL)
-      .get(
-        "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations"
-      )
+      .get("six-nations")
       .then(({ _body }) => {
         const data = _body.tournaments[0].stages[0].rounds[0].participants;
         const participantsLength = data.length;
@@ -37,9 +34,7 @@ describe("Scenario 2: Validate the ID and participant count", () => {
 describe("Scenario 3: Use an alternative competiton name", () => {
   test("should return data for an alternative competition", () => {
     return request(BASE_URL)
-      .get(
-        "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:womens-six-nations"
-      )
+      .get("womens-six-nations")
       .expect(200)
       .then(({ _body }) => {
         const data = _body.tournaments[0].name;
@@ -94,9 +89,7 @@ describe("Scenario 3: Use an alternative competiton name", () => {
 describe("Scenario 4: Invalid competition name", () => {
   test("should return 404 and error message when the URL contains an invalid competition name", () => {
     return request(BASE_URL)
-      .get(
-        "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:invalid-comp"
-      )
+      .get("invalid-comp")
       .expect(404)
       .then(({ _body }) => {
         const errorMessage = _body.error.message;
@@ -111,9 +104,7 @@ describe("Scenario 4: Invalid competition name", () => {
 describe("Scenario 5: Check that the custom header is not returned in the response", () => {
   test("should send x-test-harness header but won't return it in the response", () => {
     return request(BASE_URL)
-      .get(
-        "/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations"
-      )
+      .get("six-nations")
       .expect(200)
       .set("x-test-harness", "true")
       .then((res) => {
